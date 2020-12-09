aa7a6ee16daf4f7ea7fe1271f95d4d735f4f9e68
Shiraz Ali <shirazali1997@gmail.com>
2020-12-09 21:12:26 +0530

    Create test.py

---
+++ yelpscraper/Restaurant/test.py
@@ -0,0 +1,25 @@
+from requests import get
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+import pandas as pd
+import time
+
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument('--disable-dev-shm-usage')
+# driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)
+driver = webdriver.Chrome('/home/dhruv034/Desktop/codemarket/chromedriver_linux64/chromedriver',chrome_options=chrome_options)
+#driver = webdriver.Chrome('E:/Codes/chromedriver.exe')#,chrome_options=chrome_options)
+
+url = "https://www.yelp.com/search?find_desc=Indian+food&find_loc=Redondo+Beach%2C+CA"
+driver.get(url)
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
+WebDriverWait(driver, 10).until(lambda driver: driver.find_element_by_link_text('Start Order')).click()
+
