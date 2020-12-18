0db9fb9f6dde77df542db756f1e93ab1a9505ff2
Shiraz Ali <shirazali1997@gmail.com>
2020-12-18 23:28:47 +0530

    updated menu_scraper.py acc to requirements

--- yelpscraper/Restaurant/menu_scraper.py
+++ yelpscraper/Restaurant/menu_scraper.py
@@ -1,26 +1,18 @@
-from requests import get
+import requests 
 from bs4 import BeautifulSoup
-import re
-import os
-import time
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 import pymongo
 from mongoengine import *
 from mongoengine.context_managers import switch_collection
 import urllib.parse
 import argparse
 import datetime
-from sys import stdout
 import pandas as pd
 
 class Website(EmbeddedDocument):
-    business_name = StringField(max_length=250, required=True)
-    website_link = StringField(max_length=250, required=True)
-
+    restaurant_name = StringField(max_length=250, required=True)
+    city = StringField()
+    keyword = StringField(max_length=250)
+    menu_data = []
 
 class Menu_scraper(Document):
     userid = StringField(max_length=120, required=True)
@@ -30,21 +22,21 @@ class Menu_scraper(Document):
     keywords = ListField(StringField(max_length=250))
     created_timestamp = DateTimeField()
     last_updated = DateTimeField()
-    # collection_of_menu_scraped = EmbeddedDocumentListField(Website)
+    collection_of_menu_scraped = EmbeddedDocumentListField(Website)
 
 class Scraper:
     def __init__(self,userid,name,keyword,city):
         self.userid = userid
         self.name = name
         self.keyword = keyword
-        self.collections = "Yelp"
+        self.collections = "Menu"
         self.city = city
-        # self.all_websites = []
-        # self.final_result = set()
-        # try:
-            # self.get_scraped_data()
-        # except:
-            # print(f"{self.userid} has no data available for {self.name}")
+        self.all_websites = []
+        self.final_result = set()
+        try:
+            self.get_scraped_data()
+        except:
+            print(f"{self.userid} has no data available for {self.name}")
 
     def create_db(self, collection, query):
         status = 'Scraping Started'
@@ -56,8 +48,8 @@ class Scraper:
             document = {'userid':self.userid,
                         'name':self.name,
                         'keywords':[self.keyword],
-                        'city':self.city,
-                        'status': status
+                        'city':[self.city],
+                        'status': status,                    
                     }
             collection.insert_one(document)
     
@@ -72,80 +64,97 @@ class Scraper:
             # print(data_menu)
             dataframe = pd.DataFrame(data_menu)
             # print(dataframe)
-            col1 = dataframe.business_name.to_list()
-            col2 = dataframe.website_link.to_list()
-            # print(col1, col2,)
-            self.all_websites = col1
+            col = dataframe.restaurant_name.to_list()
+            self.all_websites = col
+            
 
-    def get_url(self):
-        # https://www.yelp.com/search?find_desc=Indian+Food&find_loc=Redondo+Beach%2C+CA
+    def get_url(self, start=0):
+        # https://www.yelp.com/search?find_desc=Indian+Food&find_loc=Redondo+Beach%2C+CA&ns=1&start={start}
         desc = self.keyword
         loc = self.city
-        url = f'https://www.yelp.com/search?find_desc={desc}&find_loc&={loc}'
+        url = f'https://www.yelp.com/search?find_desc={desc}&find_loc&={loc}&ns=1&start={start}'
         return url
 
     def scrape(self, Menu_scraper):
-        self.flag = 0
         print('Begin Scraping')
         connect(db = 'codemarket_shiraz', host = 'mongodb+srv://sumi:'+urllib.parse.quote_plus('sumi@123')+'@codemarket-staging.k16z7.mongodb.net/codemarket_shiraz?retryWrites=true&w=majority')
-        while self.flag < 10:
-            chrome_options = Options()
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--no-sandbox")
-            chrome_options.add_argument("--disable-gpu")
-            chrome_options.add_argument('--disable-dev-shm-usage')
-            # driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)
-            driver = webdriver.Chrome('/home/dhruv034/Desktop/codemarket/chromedriver_linux64/chromedriver',chrome_options=chrome_options)
-            #driver = webdriver.Chrome('E:/Codes/chromedriver.exe')#,chrome_options=chrome_options)
-            try:
-                with switch_collection(Menu_scraper, self.collections) as Menu_scraper:
-                    if self.flag == 0:
-                        Menu_scraper.objects(userid = self.userid, name = self.name).update(push__keywords = self.keyword)
-                        # Explicit wait for the site to load for slow internet connection
-                        wait = WebDriverWait(driver, 10)
-                        # Get the url using keyword and city
-                        url = self.get_url()
-                        # print(url)
-                        driver.get(url)
-                        html = driver.page_source
-                        soup = BeautifulSoup(html, 'html.parser')
-                        # Find the business(Restaurant) list from the searched url 
-                        rest_list = soup.find('ul',class_="undefined list__09f24__17TsU")
-                        lilist = rest_list.findChildren(['li'])
-                        for li in lilist:
-                            link = li.find('a',class_="link__09f24__1kwXV link-color--inherit__09f24__3PYlA link-size--inherit__09f24__2Uj95")
-                            if link == None:
-                                continue
-                            driver.get("https://yelp.com/" + link['href'])
-                            rest_page = driver.page_source
-                            rest_soup = BeautifulSoup(rest_page, 'html.parser')
-                            rest_name = rest_soup.find('h1',class_ = "lemon--h1__373c0__2ZHSL heading--h1__373c0__dvYgw undefined heading--inline__373c0__10ozy").text
-                            print(rest_name)
+        with switch_collection(Menu_scraper, self.collections) as Menu_scraper:
+            data = []
+            for start in range (0, 10, 10):
+                Menu_scraper.objects(userid = self.userid, name = self.name).update(push__keywords = self.keyword)
+                url = self.get_url(start)
+                source = requests.get(url).text
+                soup = BeautifulSoup(source, 'html.parser')
+                for i in soup.findAll("a", {"class": "link__09f24__1kwXV link-color--inherit__09f24__3PYlA link-size--inherit__09f24__2Uj95"}):
+                    if 'ad_business_id' not in i['href']:
+                        link = "https://www.yelp.com/"+i['href']
+                        menu_link = link.split('/')
+                        menu_link[4] = '/menu/'
+                        menu_link[1] = '//'
+                        menu_link = ''.join(menu_link)
+                        menu_page = menu_link 
+                        # print(link)
+                        # print(menu_link)
+                        status = 'Scraping Restaurant Menu'
+                        Menu_scraper.objects(userid = self.userid, name = self.name).update(set__status = status)
+                        menu_url = requests.get(menu_link)
+                        menu_soup = BeautifulSoup(menu_url.content, 'lxml')
+                        hotel_soup  = BeautifulSoup(menu_url.content, 'lxml')
+                        restaurantName = hotel_soup.find('h1')
+                        if restaurantName is None:
+                            continue 
+                        restaurantName = restaurantName.text.strip()
+                        restaurantName = restaurantName.strip('Menu for ')
+                        # print(restaurantName)
+                        if restaurantName in self.all_websites:
+                            print("Restaurant data already available")
+                        else:
+                            self.all_websites.append(restaurantName)
+                            menu = menu_soup.find("div", {"class": "menu-sections"})
+                            h2 = menu.find_all("h2")
+                            for h in h2:
+                                section = h.text.strip()
+                                items = h.parent.find_next_sibling()
+                                h4 = items.find_all("h4")
+                                for h44 in h4:
+                                    itemName=h44.text.strip()
+                                    price =h44.parent.find_next_sibling()
+                                    itemPrice = price.find("li", {"class", "menu-item-price-amount"})
+                                    
+                                    if itemPrice is None:
+                                        itemPrice = price.findAll('tr')#container.findAll('tr')
+                                        l1=[]
+                                        
+                                        for p in itemPrice:
+                                            size = p.find('th').text.strip()
+                                            pr = p.find('td').text.strip()
+                                            l1.append(size + ' - ' + pr)
+                                            itemPrice = ' , '.join(l1)
+                                    else:
+                                        itemPrice=itemPrice.text.strip()
+                                    
+                                    if len(itemPrice) == 0:
+                                        itemPrice = ""
+                                    
+                                    data.append({"Restaurant":restaurantName, "Category":section, "Item":itemName,"Price":itemPrice})
+                                    # print({"Hotel":restaurantName, "category":section, "item":itemName,"price":itemPrice})
+                                    website_object = Website()
+                                    website_object.restaurant_name = restaurantName
+                                    website_object.keyword = self.keyword
+                                    website_object.city = self.city
+                                    website_object.menu_data = data
 
-                            # Click on the Takeout tab
-                            tab = wait.until(EC.element_to_be_clickable((
-                                By.XPATH, '//div[@class="lemon--div__373c0__1mboc tab__373c0__24QGW tabNavItem__373c0__3X-YR tab--section__373c0__3V0A9 tab--no-outline__373c0__3adQG"]'
-                            )))
-                            tab.click()
-                            # Click on the Start Order button
-                            order = wait.until(EC.element_to_be_clickable((
-                                By.XPATH, '//button[@class="button__373c0__3lYgT primary__373c0__2ZWOb full__373c0__1AgIz"]'
-                            )))
-                            order.click()
-                            status = 'Scraping website'
-                            Menu_scraper.objects(userid = self.userid, name = self.name).update(set__status = status)
-                            time.sleep(3)
-                            menu_link = driver.current_url
-                            menu_page = driver.page_source
-                            menu_soup = BeautifulSoup(menu_page, 'html.parser')
-                            print("Parsed cart page")
-                    
-                    Menu_scraper.objects(userid = self.userid, name = self.name).update(set__status = "Scraping Completed")
-                    break
+                                    try:
+                                        Menu_scraper.objects(userid = self.userid, name = self.name).update(push__collection_of_menu_scraped = website_object)
+                                        Menu_scraper.objects(userid = self.userid, name = self.name).update(set__last_updated = datetime.datetime.now())
+                                    except:
+                                        print("Not Unique data")
+            # print(data)
+            dataset = pd.DataFrame(data,columns=['Restaurant', 'Category', 'Item', 'Price'])
+            # print(dataset)
 
-            except AttributeError:
-                self.flag += 1
-                print(f"trial:{self.flag}")
+        Menu_scraper.objects(userid = self.userid, name = self.name).update(set__status = "Scraping Completed")
+                   
         print('End Scraping')
 
 if __name__ == '__main__':
