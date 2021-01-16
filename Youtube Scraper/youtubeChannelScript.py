85e5f5b2a5cb5d60a3eea4cac6b8431df4e26e5f
nooras <anoorasfatima@gmail.com>
2021-01-16 18:30:49 +0530

    Youtube Script Updated and Resolved Some error

--- Youtube Scraper/youtubeChannelScript.py
+++ Youtube Scraper/youtubeChannelScript.py
@@ -16,12 +16,27 @@ import csv
 import pymongo
 from bson.objectid import ObjectId
 import sys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+
 
 #Use Incognito mode when scraping
 chrome_options = Options()
 chrome_options.add_argument(" — incognito")
-browser = webdriver.Chrome(options=chrome_options)
+chrome_options.add_argument('--headless')
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument('--disable-infobars')
+# chrome_options.add_argument("start-maximized")
+# chrome_options.add_argument("disable-infobars")
+# chrome_options.add_argument("--disable-extensions")
+# chrome_options.add_argument('--remote-debugging-port=9222')
+browser = webdriver.Chrome(r'/usr/local/bin/chromedriver', options=chrome_options)
 
+print("Scrapping Started.")
 #Search and getting the page data
 # SearchWord = "cancer"
 SearchWord = sys.argv[1] 
@@ -38,11 +53,14 @@ for a in aTag:
     videoTitle = a.get_attribute("title")
     videoHref = a.get_attribute("href")
     aAttribute[videoTitle] = videoHref
+#print(aAttribute)
 
 #Function for getting channel Name and Channel href
 def browseChannelUrl(urlBrowse):
+    sleep(2)
     browser.get(urlBrowse)
     browser.page_source
+    sleep(3)
     channel = browser.find_element_by_xpath('//*[@id="text"]/a')
     channelName = channel.text
     channelHref = channel.get_attribute('href')
@@ -55,12 +73,17 @@ def browseChannelUrl(urlBrowse):
 #Function for Retrieving all link from about page
 def aboutRetrieveUrl(urlChannel):
     #link = {}
+    sleep(2)
     browser.get(urlChannel)
     browser.page_source
+    sleep(2)
     text_list = browser.find_elements_by_xpath("//div[@class='tab-content style-scope paper-tab']")  
     btn = text_list[-1]    # Fetching the text of last element
-    btn.click()
+    # btn.click()
+    ActionChains(browser).click(btn).perform()
+    # WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, "(//div[@class='tab-content style-scope paper-tab'])[last()]"))).click()
     browser.page_source
+    sleep(2)
     data = browser.find_element_by_xpath('//*[@id="links-container"]') 
     aTags = data.find_elements_by_tag_name('a')
     l = [{"Link_Name":a.text, "Link":a.get_attribute('href')} for a in aTags]
@@ -86,13 +109,18 @@ with open('YTScriptData.csv', mode='w') as csv_file:
         videoTitle = key
         videoHref = value
         sleep(1)
+        #print(videoTitle, videoHref)
+        if videoHref is None:
+            continue
         chanelName, channelHref = browseChannelUrl(videoHref)
+        #print(chanelName, channelHref)
         sleep(1)
         aboutLinks = aboutRetrieveUrl(channelHref)
         sleep(1)
         #print(videoTitle, videoHref)
-        #print(chanelName, channelHref)
         #print(aboutLinks)
         writer.writerow({'VideoTitle': videoTitle, 'VideoHref': videoHref, 'ChannelName': chanelName, 'ChannelHref':channelHref, 'Links':aboutLinks}) #inserting in csv file
         YTData.insert({'VideoTitle': videoTitle, 'VideoHref': videoHref, 'ChannelName': chanelName, 'ChannelHref':channelHref, 'Links':aboutLinks}, check_keys=False) #Inserting in db
         print("-----One Document Inserted.------")
+print("Scrapping Stopped.")
+
