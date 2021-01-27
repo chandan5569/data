612c8e856a5aa14ba7a2f1c468e3ed2cf4badc79
Bilal <bilalmomin39@gmail.com>
2021-01-27 15:27:01 +0530

    linkedin reply

--- linkedinreply/linkedin_reply.py
+++ linkedinreply/linkedin_reply.py
@@ -60,10 +60,10 @@ def chat_scroll():
 
 
 chrome_options = Options()
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument("--disable-gpu")
-# chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 # PATH= r"/usr/local/bin/chromedriver"
 PATH = r"C:\Users\BILAL\Projects\LinkedInScraper\chromedriver.exe"
