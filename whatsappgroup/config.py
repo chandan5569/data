beef9b9d2ae9fe952cf0d05c44a120afd7c45c87
crystalclear42 <47495981+crystalclear42@users.noreply.github.com>
2021-02-04 13:43:44 +0530

    Delete whatsappgroup directory

--- whatsappgroup/config.py
+++
@@ -1,6 +0,0 @@
-CHROME_PROFILE_PATH = "user-data-dir=C:\\Users\\Nishith\\AppData\\Local\\Google\\Chrome\\User Data\\Wtsp"
-
-
-# Windows 7, 8.1, and 10: C:\Users\<username>\AppData\Local\Google\Chrome\User Data\Default
-# MAC OS X El Capitan: Users/<username>/Library/Application Support/Google/Chrome/Default
-# Linux: /home/<username>/.config/google-chrome/default
